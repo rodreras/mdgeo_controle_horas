623026bfd32086c09b8108bd4e7f4ad63e76653a
rodrigo <rodrigo@pop-os.localdomain>
2021-11-24 14:35:15 -0300

    updating app.py
    removed plt import

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st 
 import pandas as pd 
 import numpy as np 
-import matplotlib.pyplot as plt 
 from datetime import date, datetime, timedelta
 import controllers.HoursController as HoursController
 import models.Hours as hours 
